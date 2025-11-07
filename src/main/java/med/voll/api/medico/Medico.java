d93693710fe4a96525d75140c1f7919392caeec1
edualvesz <eduardo_ean@outlook.com>
2025-11-07 11:56:51 -0300

    Primeiro commit - API Voll Med

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,16 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public class Medico 
+{
+    
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private DadosEndereco endereco;
+
+
+}
