808bf12243083204b328467b098e49e396facfcc
edualvesz <eduardo_ean@outlook.com>
2025-11-08 12:06:52 -0300

    feat: add migration for medicos table and annotate MedicoController with @Transactional

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +17,13 @@ import med.voll.api.medico.MedicoRepository;
 
 public class MedicoController 
 {
+    // automaticamente cria uma instancia do repository e injeta aqui
     @Autowired
     private MedicoRepository repository;
-    
+    //post é usado para criar recursos
     @PostMapping
+    //transactional garante que se der algum erro na hora de salvar, ele desfaz tudo que foi feito no banco até aquele ponto
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
