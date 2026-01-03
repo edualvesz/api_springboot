75e31586427463de232842932cd8638d55cb5537
eduardo-dev84 <enascimento80@gmail.com>
2026-01-03 18:34:14 -0300

    feat: add patient controller endpoints

---
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,22 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        DadosEndereco endereco
+) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(
+            paciente.getId(),
+            paciente.getNome(),
+            paciente.getEmail(),
+            paciente.getTelefone(),
+            new DadosEndereco(paciente.getEndereco())
+        );
+    }
+}
